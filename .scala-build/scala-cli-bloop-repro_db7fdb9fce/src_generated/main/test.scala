64b8e4d5c48c37b9fcf1e8dd9c35eef1553b168f
Jeff Lewis <lewisjkl@me.com>
2026-02-04 13:15:58 -0700

    test

---
+++ .scala-build/scala-cli-bloop-repro_db7fdb9fce/src_generated/main/test.scala
@@ -0,0 +1,31 @@
+
+
+final class test$_ {
+def args = test_sc.args$
+def scriptPath = """test.sc"""
+/*<script>*/
+println("Hello!")
+
+/*</script>*/ /*<generated>*//*</generated>*/
+}
+
+object test_sc {
+  private var args$opt0 = Option.empty[Array[String]]
+  def args$set(args: Array[String]): Unit = {
+    args$opt0 = Some(args)
+  }
+  def args$opt: Option[Array[String]] = args$opt0
+  def args$: Array[String] = args$opt.getOrElse {
+    sys.error("No arguments passed to this script")
+  }
+
+  lazy val script = new test$_
+
+  def main(args: Array[String]): Unit = {
+    args$set(args)
+    val _ = script.hashCode() // hashCode to clear scalac warning about pure expression in statement position
+  }
+}
+
+export test_sc.script as `test`
+
